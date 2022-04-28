53c2baa41551730061101daa988a1119d37a187f
Ivan Milic <milic.tg@gmail.com>
2022-04-28 12:49:23 +0200

    Company info in progress

---
+++ app/src/main/java/com/delminiusdevs/stockmarketapp/domain/model/CompanyInfo.kt
@@ -0,0 +1,10 @@
+package com.delminiusdevs.stockmarketapp.domain.model
+
+
+data class CompanyInfo(
+    val symbol: String,
+    val description: String,
+    val name: String,
+    val country: String,
+    val industry: String
+)
